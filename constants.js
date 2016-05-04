0cefa57854c8d884fcd256fa2435bf3c2b56d3cd
dicristomanuel <manuel.dicristo@raise.com>
2016-05-04 09:55:35 -0500

    [bot.js] Fix multiline botWords

--- constants.js
+++ constants.js
@@ -1,9 +1,7 @@
-export const defaultUser  = 'consumer';
+export const defaultUser  =  'consumer';
 
-export const botGreetings = `hello|hi|hey|hola|howdy|good morning|
-                             good evening|good afternoon`;
+export const botGreetings =  'hello|hi|hey|hola|howdy|good morning|good evening|good afternoon|';
 
-export const botBrands    = `walmart|target|home depot|macy\'s|kmart|
-                             taco bell|whole foods`;
+export const botBrands    =  'walmart|target|home depot|macy\'s|macys|kmart|taco bell|whole foods|';
 
-export const botWords     = botGreetings + botBrands;
+export const botWords     =   botGreetings + botBrands;
