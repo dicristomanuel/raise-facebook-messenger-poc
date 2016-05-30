ed427e8ddf02ebd407583dcb3d2b8ae1755e48b9
dicristomanuel <manuel.dicristo@raise.com>
2016-05-29 19:28:05 -0500

    [prism] First switch logic option

--- prism/prism.js
+++ prism/prism.js
@@ -4,18 +4,20 @@ export default {
   },
   addState: function(state) {
     this[state.to] = {
-      on: (data) => {},
-      switch: (newState, data) => { this.callState(newState, data) },
-      off: (data) => {},
+      on: state.on,
+      switch: (data) => {
+        return new Promise((resolve, reject) => {
+          data.switch.execute ? reject(this.callState(data.switch.newState, data)) : resolve(data);
+        });
+      },
+      off: state.off,
     }
     this.nextState.push({current: state.from, next: state.to});
   },
   callState: function(state, data) {
     this[state].on(data)
-    .then(function(data) {
-      debugger;
-      this[state][data.next](data);
-    });
+    .then(this[state].switch(data))
+    .then(this[state].off(data));
   },
   next: function(currentState, data) {
     this.nextState.forEach(this.findNextState(currentState, data), this);
