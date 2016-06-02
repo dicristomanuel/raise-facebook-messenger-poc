5ea6d1b2ffb892eab394689161d9c4dcd8f7e931
dicristomanuel <manuel.dicristo@raise.com>
2016-06-02 12:49:07 -0500

    [React+] Add react-redux module and STATE in action filter

--- react/createStore.js
+++ react/createStore.js
@@ -1,6 +1,5 @@
 import { createStore } from 'redux';
 import ChatApp from './reducers/index';
-// import { AddChat, UpdateStatus, SetVisibilityFilter, VisibilityFilters } from './actions';
 
 const store = createStore(ChatApp);
 
