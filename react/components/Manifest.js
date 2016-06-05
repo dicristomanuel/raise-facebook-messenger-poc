7fd8584f925009be4db9c74a15679730c39fcc32
dicristomanuel <manuel.dicristo@raise.com>
2016-06-04 20:06:51 -0500

    [UI] Add consitional status css classes

--- react/components/Manifest.js
+++ react/components/Manifest.js
@@ -1,15 +1,20 @@
 import React, { PropTypes } from 'react';
 
-const Manifest = (chat) => (
-  <li className='manifest'>
-    <div className='state'>
-      <div className='profile-pic'>
-        <img src={chat.profilePic} />
+const Manifest = (chat) => {
+
+  let status = chat.busy ? 'busy' : 'solved';
+  let state = `state ${status}`;
+  return (
+    <li className='manifest'>
+      <div className={state}>
+        <div className='profile-pic'>
+          <img src={chat.profilePic} />
+        </div>
       </div>
-      </div>
-      <p className="name">{chat.name}</p>
-  </li>
-);
+    <p className="name">{chat.name}</p>
+    </li>
+  );
+}
 
 Manifest.PropTypes = {
   chat: PropTypes.object.isRequired
