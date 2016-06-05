6ede13d2cea425e8d9391234b31beab56f7a2c2c
dicristomanuel <manuel.dicristo@raise.com>
2016-06-05 15:42:29 -0500

    [UI: Link] Style

--- react/components/Link.js
+++ react/components/Link.js
@@ -2,8 +2,8 @@ import React, { PropTypes } from 'react';
 
 const Link = ({ active, children, onClick }) => {
   // TODO: ask how does this highlight the right filter?
-  if (active)
-  return <span>{children}</span>
+  // if (active)
+  // return <span>{children}</span>
 
   return (
     <a href='#'
