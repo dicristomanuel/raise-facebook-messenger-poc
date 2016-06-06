6af7e8d09ec98fed9ed74939e8a1074e069d25a5
dicristomanuel <manuel.dicristo@raise.com>
2016-06-05 22:49:50 -0500

    [UI: Header] Css style

--- react/components/Header.js
+++ react/components/Header.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ChatFilter from './ChatFilter';
-
+import ChatFilter from './NameFilter';
 const Header = () => (
   <div className='header'>
     <div className='logo'>
       <img src='images/raise.png'/>
     </div>
     <ChatFilter />
+    <NameFilter />
+    // ADD Name filter for fuzzy search
   </div>
 );
 
