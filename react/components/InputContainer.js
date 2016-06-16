774fd91378f5aa05ba48895a55b4c470f8b985eb
dicristomanuel <manuel.dicristo@raise.com>
2016-06-16 14:17:20 -0500

    [InputContainer] Restore dynamic textarea height

--- react/components/InputContainer.js
+++ react/components/InputContainer.js
@@ -3,7 +3,7 @@ import { findDOMNode } from 'react-dom';
 
 class InputContainer extends Component {
   handleChange(event) {
-    const element = findDOMNode(this).childNodes[0];
+    const element = findDOMNode(this).childNodes[0].childNodes[0];
     element.style.height = 'auto';
     element.style.height = `${element.scrollHeight}px`;
   }
@@ -26,10 +26,22 @@ class InputContainer extends Component {
           <li>
             <div className='xsm-main-container'>
               <div className='xsm-single-container' onClick={this.onClick}>
-                <img src='https://scontent.xx.fbcdn.net/v/t1.0-1/p200x200/12801477_685706248238148_6224624586905789762_n.jpg?oh=ca5e3916fdacd7b753c62ab02eb9b6fc&oe=57C37846' className='xsm'/>
+                <div className='dot'></div>
+                <div className='img-container'>
+                  <img src='https://scontent.xx.fbcdn.net/v/t1.0-1/p200x200/12801477_685706248238148_6224624586905789762_n.jpg?oh=ca5e3916fdacd7b753c62ab02eb9b6fc&oe=57C37846' className='xsm'/>
+                </div>
               </div>
               <div className='xsm-single-container bounce' onClick={this.onClick}>
-                <img src='https://scontent.xx.fbcdn.net/v/t1.0-1/p200x200/12801477_685706248238148_6224624586905789762_n.jpg?oh=ca5e3916fdacd7b753c62ab02eb9b6fc&oe=57C37846' className='xsm'/>
+                <div className='dot'></div>
+                <div className='img-container'>
+                  <img src='https://scontent.xx.fbcdn.net/v/t1.0-1/p200x200/12801477_685706248238148_6224624586905789762_n.jpg?oh=ca5e3916fdacd7b753c62ab02eb9b6fc&oe=57C37846' className='xsm'/>
+                </div>
+              </div>
+              <div className='notification-container notification-animation-in'>
+                <div className='notification engaged'>
+                  <p className='notification-text'>Chat engaged</p>
+                  <p className='notification-undo'>UNDO</p>
+                </div>
               </div>
             </div>
           </li>
@@ -38,9 +50,5 @@ class InputContainer extends Component {
     )
   }
 };
-
-// <a href='#'>
-// <img src="/assets/images/dots.png" className="options-message"></img>
-// </a>
 
 export default InputContainer;
