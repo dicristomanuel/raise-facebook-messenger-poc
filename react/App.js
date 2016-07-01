e8f87e1c8399ae4adf56b4098eaa9e80982f3c9b
dicristomanuel <manuel.dicristo@raise.com>
2016-06-30 21:23:35 -0500

    [App] refactor

--- react/App.js
+++ react/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import Routes from './config/routes';
-import { InitChatsAndSockets } from './helpers/chatAllHelper';
 
 ReactDOM.render(
   <Router history={browserHistory} routes={Routes} />,
